a6ca2677b3f71c3bb55986c66334064d43878b1b
treeder <treeder@gmail.com>
2025-10-22 15:21:17 +0000

    cleanup

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,49 +1,22 @@
-import { nanoid } from 'nanoid'
-import { ConsoleLogger } from 'console-logger'
-import { D1, ErrorHandler, CloudflareLogger } from 'flaregun'
 import { Rend } from 'rend'
 import { layout } from './layout.js'
-import { init } from './_once.js'
+import { init, initRequest } from './init.js'
 import { once } from 'once'
 import { cors } from 'flaregun/middleware/cors.js'
 import { timer } from 'flaregun/middleware/timer.js'
 
-const errorHandler = new ErrorHandler()
 export async function wrap(c) {
+  await initRequest(c)
   try {
-    let req = c.request
-
-    // setup logger
-    let rid = nanoid()
-    let url = new URL(req.url)
-    let logger = new ConsoleLogger({ data: { requestId: rid, path: url.pathname } })
-    if (c.env.ENV == 'prod') {
-      logger = new CloudflareLogger({ data: { requestId: rid, path: url.pathname } })
-    }
-    c.data.logger = logger
-
-    if (url.pathname.includes('.')) {
-      // skip static assets
-      return await c.next()
-    }
     await once(init, c)
-
-    // setup env vars
-    c.data.env = c.env.ENV
-    // c.data.someAPI = JSON.parse(c.env.SOME_API)
-
-    c.data.d1 = new D1(c.env.D1)
-    c.data.kv = c.env.KV
-    c.data.r2 = c.env.R2
 
     c.data.rend = new Rend({
       layout,
     })
 
-    let r = await c.next()
-    return r
+    return await c.next()
   } catch (err) {
-    return await errorHandler.handle(c, err)
+    return await c.data.errorHandler.handle(c, err)
   }
 }
 
--- functions/scheduled.js
+++ functions/scheduled.js
@@ -1,9 +1,5 @@
-// https://developers.cloudflare.com/workers/runtime-apis/handlers/scheduled/
-import { getProducts, Product } from './data/products.js'
-import { D1, Scheduler } from 'flaregun'
-import { globals } from './globals.js'
 import { once } from 'once'
-import { init } from './_once.js'
+import { init, initRequest } from './init.js'
 
 /**
  * Be aware that no middleware or anything will run here.
@@ -12,5 +8,9 @@ import { init } from './_once.js'
  */
 export async function scheduled(c) {
   let st = new Date(c.controller.scheduledTime)
-  console.log('scheduled:', c.controller.cron, c.controller.type, c.controller.scheduledTime, st)
+  console.log('scheduled:', c.controller.cron, st)
+  await initRequest(c)
+  await once(init, c)
+
+  await c.data.globals.scheduler.run(c, c.controller)
 }
