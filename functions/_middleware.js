4f5cb4bef7e4b78696b7764aa2720f3fd013202b
Travis Reeder <treeder@gmail.com>
2026-07-06 21:36:48 -0700

    Check auth in _middleware.js and set c.data.user

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -5,13 +5,31 @@ import { once } from 'once'
 import { cors } from 'flaregun/middleware/cors.js'
 import { timer } from 'flaregun/middleware/timer.js'
 
+import { getSession } from 'passkeys/src/sessions.js'
+import { User } from './data/users.js'
+
 export async function wrap(c) {
   await initRequest(c)
   try {
     await once(init, c)
 
+    // Check auth and set c.data.user if logged in
+    const sess = await getSession({ request: c.request, kv: c.env.KV })
+    if (sess && sess.userId) {
+      let user = await c.data.d1.get(User, sess.userId)
+      if (!user) {
+        user = { id: sess.userId, email: sess.email }
+      }
+      c.data.user = user
+    }
+
     c.data.rend = new Rend({
       layout,
+      data: {
+        get user() {
+          return c.data.user
+        },
+      },
     })
 
     return await c.next()
